ed11bcfb2d3a53c080757dfda23ba66e2b9e059a
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2022-01-25 13:24:02 -0300

    Atualizando e fazendo algumas validações proposta como desafios

---
+++ pages/chat.js
@@ -0,0 +1,10 @@
+
+function PaginaDoChat (){
+    return(
+        <div>
+            Página para o Chat
+        </div>
+    );
+}
+
+export default  PaginaDoChat;
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,8 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -50,10 +23,11 @@ function Titulo(props) {
 export default function PaginaInicial() {
 
   const [username, setUserName] = useState('RicardoSantanaEvangelista'); 
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
+
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -81,6 +55,16 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={ function (eventInfo){
+                eventInfo.preventDefault();
+                if(username.length > 2){
+                  roteamento.push('/chat');
+                }else{
+                  alert("[ERRO] Por favor, preencha os campos corretamente !");
+                }
+               
+                
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px'
